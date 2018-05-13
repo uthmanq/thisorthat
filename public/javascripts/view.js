4ee217688266a27ff5779f70d83efb8222812a0f
UthmanQureshi <uthman.aq@gmail.com>
2018-05-13 00:30:49 -0400

    update front page and view-page

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -1,16 +1,10 @@
 function viewPost (_id){
-    console.log(_id);
-    $.ajax({
-        url: "/posts/view",
-        type: "get", //send it through get method
-        data: { 
-          _id: _id
-        },
-        success: function(response) {
-          //Do Something
-        },
-        error: function(xhr) {
-          //Do Something to handle error
-        }
-      });
+      window.location.href = "/posts/view?_id=" + _id;
+}
+
+function fixDate (date)
+{
+      var d = new Date(date);
+      var result = d.getDay;
+      return result;
 }
